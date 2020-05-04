add4a337f09528b71b6cc427b831ef642c1b9960
michodgs24 <michodgs@gmail.com>
2020-05-04 23:03:16 +0000

    Remove API key, prevent security breaches

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,14 +1,12 @@
-  
-var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var labelIndex = 0;
 var map;
 var InforObj = [];
 
 var markersOnMap = [
     {
-        placeName: "Cheddar Gorge"<br><a href='https://goo.gl/maps/AIzaSyDwkm-Zl_LpT63IFWXzmGhS8i6vZFf9dM8'>Get Directions and more information</a>,
+        placeName: "Cheddar Gorge"<br><a href='https://goo.gl/maps/api_key'>Get Directions and more information</a>,
         LatLng: [{
             lat: 51.2880522,
             lng: -2.7575527
-        }]
-    },
+        }],
