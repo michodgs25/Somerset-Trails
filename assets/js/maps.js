0f0ddf22b9f60509512286931291a57785a6f2f8
michodgs24 <michodgs@gmail.com>
2020-05-04 22:22:53 +0000

    Add google maps scripts to html document.

---
+++ assets/js/maps.js
@@ -0,0 +1,14 @@
+  
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labelIndex = 0;
+var map;
+var InforObj = [];
+
+var markersOnMap = [
+    {
+        placeName: "Cheddar Gorge"<br><a href='https://goo.gl/maps/AIzaSyDwkm-Zl_LpT63IFWXzmGhS8i6vZFf9dM8'>Get Directions and more information</a>,
+        LatLng: [{
+            lat: 51.2880522,
+            lng: -2.7575527
+        }]
+    },
