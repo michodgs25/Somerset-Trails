d419715e5cdde730e43c183e032e6e0789ce0ecd
michodgs24 <michodgs@gmail.com>
2020-05-08 13:16:39 +0000

    Update homepage slideshow images

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -7,7 +7,7 @@ function plusDivs(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("home-slides");
+  var x = document.getElementsByClassName("slide");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
