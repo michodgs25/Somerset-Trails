14ff295e263d3c984513dcc6c695eb80fbe3a0fe
michodgs24 <michodgs@gmail.com>
2020-05-08 14:12:50 +0000

    Add home-map section, slideshow buttons

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,3 +1,10 @@
+/*Homepage map*/
+
+
+
+
+
+/*
 var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var labelIndex = 0;
 var map;
@@ -5,8 +12,8 @@ var InforObj = [];
 
 var markersOnMap = [
     {
-        placeName: "Cheddar Gorge"<br><a href='https://goo.gl/maps/api_key'>Get Directions and more information</a>,
-        LatLng: [{
+        placeName: "Cheddar Gorge" < br > <a href='https://goo.gl/maps/api_key'>Get Directions and more information</a>,
+        LatLng: {
             lat: 51.2880522,
             lng: -2.7575527
-        }],
+        };
