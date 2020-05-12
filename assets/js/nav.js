6535fee966ec8ee1be7e42d710507356ab03c38b
michodgs24 <michodgs@gmail.com>
2020-05-12 21:42:27 +0000

    Edit code format& css styling of homepage navigation

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,27 +1,8 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementsByClassName("topnav");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
-// Get dropdown
-
-function myFunction() {
-  var x = document.getElementById("myTopnav");
+  var x = document.getElementsByClassName("topnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
