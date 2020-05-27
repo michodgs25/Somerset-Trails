0ade5d736b91d84543e5527874ffcff53c2a782d
michodgs24 <michodgs@gmail.com>
2020-05-27 22:13:52 +0000

    Add JQuery to text sections& clean html and css code formatting

--- assets/js/text.js
+++ assets/js/text.js
@@ -1,2 +1,17 @@
+/*banner text*/
+$("h4").css({"color": "white", "font-style": "italic", "text-align": "center", "padding-top": "5rem"});
 
+/*h2 headings*/
+$("h2").css({"color": "green", "text-align": "center", "padding-top": "2rem"});
 
+/*p text*/
+$("p").css({"font-style": "italic", "text-align": "center"});
+
+$("p").on({
+  mouseenter: function(){
+    $(this).css("background-color", "green", "color", "white");
+  },
+    mouseleave: function(){
+    $(this).css("background-color", "white", "color", "black");
+    }
+});
