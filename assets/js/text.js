97564885e7d80f537e8265f21b0e45367786b984
michodgs24 <michodgs@gmail.com>
2020-05-28 18:49:05 +0000

    Update JQuery text& styling

--- assets/js/text.js
+++ assets/js/text.js
@@ -16,15 +16,7 @@ $("h2").css({"color": "green", "text-align": "center", "padding-top": "2rem"});
 /*p text*/
 $("p").css({"font-style": "italic", "text-align": "center"});
 
-$("p").on({
-  mouseenter: function(){
-    $(this).css("background-color", "green", "color", "white");
-  },
-    mouseleave: function(){
-    $(this).css("background-color", "white", "color", "black");
-    }
-});
-
+/*navigation achor tags*/
 $("a").on({
     mouseenter: function(){
         $(this).css("font-size", "1.5rem");
@@ -32,4 +24,14 @@ $("a").on({
     mouseleave: function(){
         $(this).css("font-size", "1rem");
     }
-})
+});
+
+/*button*/
+$("button").on({
+    mouseenter: function(){
+        $(this).css("font-size", "2.5rem");
+    },
+    mouseleave: function(){
+        $(this).css("font-size", "2rem");
+    }
+});
