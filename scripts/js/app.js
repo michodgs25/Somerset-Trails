9040c0dcfba4488c3c6b0f88542bd259650f566c
michodgs24 <michodgs@gmail.com>
2020-06-10 12:39:28 +0000

    Add scroll event listeners, update comments and update images

--- scripts/js/app.js
+++ scripts/js/app.js
@@ -1,122 +1,143 @@
-//declare& get navigation class
+/*declare& get navigation class, increase user accessibility*/
 function dropDownNavigation() {
-  var x = document.getElementsByClassName("topnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementsByClassName("topnav");/*top navigation*/
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 function dropUpNavigation() {
-  var x = document.getElementsByClassName("bottom-nav");
-  if (x.className === "bottom-nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "bottom-nav";
-  }
+    var x = document.getElementsByClassName("bottom-nav");/*bottom navigation*/
+    if (x.className === "bottom-nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "bottom-nav";
+    }
 }
 /*end of navigation code*/
 
-//get button
+/*scroll to to top button, increase user accessibility*/
 mybutton = document.getElementById("myBtn");
 
 /*when user scrolls 10rem from top of page, show button*/
-
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
         mybutton.style.display = "block";
     } else {
-        mybutton.style.display ="none";
+        mybutton.style.display = "none";
     }
 }
 
 //when user clicks, scroll to top of document*/
-
-function topFunction () {
+function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0; /*for chrome, firefox, IE and opera*/
 }
 /*End of scroll code*/
 
+/*slideshow function,styling& animation, ensure slideshow is appealing and works effectively*/
 var slideIndex = 0;
 showSlides();
 
-/*slideshow function*/
-
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");//active dot changes with each slide
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); /* Change image every 2 seconds*/
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");//active dot changes with each slide
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000); /* Change image every 2 seconds*/
 }
 /*end of slideshow code*/
 
-/*banner text*/
-$("h4").css({"color": "white", "font-style": "italic", "text-align": "center"});
+/*banner text, introduce viewer to page and increase interactivity*/
+$("h4").css({ "color": "white", "font-style": "italic", "text-align": "center" });
 
 $("h4").on({
-    mouseenter: function(){
+    mouseenter: function () {
         $(this).css("font-size", "3rem", "z-index", "3");
     },
-    mouseleave: function(){
+    mouseleave: function () {
         $(this).css("font-size", "1.2rem", "z-index", "3");
     }
 });
+/*end of banner text*/
 
-/*h2 headings*/
-$("h2").css({"text-align": "center", "padding-top": "1rem"});
+/*h2 headings, increase visibility& interactivity*/
+$("h2").css({ "text-align": "center", "padding-top": "1rem" });
 
 $("h2").on({
-    mouseenter: function(){
+    mouseenter: function () {
         $(this).css("font-size", "2rem");
     },
-    mouseleave: function(){
+    mouseleave: function () {
         $(this).css("font-size", "1rem");
     }
 });
+/*end of h2 code*/
 
-/*navigation achor tags*/
+/*achor tags, increase interactivity*/
 $("a").on({
-    mouseenter: function(){
+    mouseenter: function () {
         $(this).css("font-size", "1.2rem");
     },
-    mouseleave: function(){
+    mouseleave: function () {
         $(this).css("font-size", "1rem");
     }
 });
+/*end of achor tags code*/
 
-/*button*/
+/*button elements, increase button interactivity*/
 $("button").on({
-    mouseenter: function(){
+    mouseenter: function () {
         $(this).css("font-size", "1.5rem");
     },
-    mouseleave: function(){
+    mouseleave: function () {
         $(this).css("font-size", "2rem");
     }
 });
+/*end of button element*/
 
-/*footer icons& text*/
+/*footer icons& text, increase footer interactivity*/
 $("span").css("padding", "0.5rem");
 
 $("span").on({
-    mouseenter: function(){
+    mouseenter: function () {
         $(this).css("font-size", "1.5rem");
     },
-    mouseleave: function(){
+    mouseleave: function () {
         $(this).css("font-size", "2rem");
     }
 });
-/*end of footer*/
+/*end of footer comment*/
+
+/*scroll event-listener, ensure smooth scroll on mobile devices*/
+let passiveIfSupported = false;
+
+try {
+    window.addEventListener("test", null,
+        Object.defineProperty(
+            {},
+            "passive",
+            {
+                get: function () { passiveIfSupported = { passive: false }; }
+            }
+        )
+    );
+} catch (err) { }
+
+window.addEventListener("scroll", function (event) {
+    /*do something*/
+    /*if browser cannot use event.preventDefault();*/
+}, passiveIfSupported);
