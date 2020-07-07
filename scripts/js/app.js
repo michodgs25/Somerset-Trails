841c9367e4b31666e307a20022949d643de26bdf
michodgs24 <michodgs@gmail.com>
2020-07-07 10:34:09 +0000

    Update js indentation, increase readability

--- scripts/js/app.js
+++ scripts/js/app.js
@@ -1,12 +1,12 @@
-/*please visit readme.md for code credits*/
+//please visit readme.md for code credits
 $(".topnav.response").removeClass("responsive").addClass("topnav");
 /*declare& get navigation class, increase user accessibility*/
 $(".bottom-nav.response").removeClass("responsive").addClass("bottom-nav");
 
-/*scroll to to top button, increase user accessibility*/
+//scroll to to top button, increase user accessibility
 mybutton = document.getElementById("myBtn");
 
-/*when user scrolls 10rem from top of page, show button*/
+//when user scrolls 10rem from top of page, show button
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
     if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
@@ -15,12 +15,14 @@ function scrollFunction() {
         mybutton.style.display = "none";
     }
 }
-//scroll to top*/
+
+//scroll to top
 function topFunction() {
     document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0; /*for chrome, firefox, IE and opera*/
+    document.documentElement.scrollTop = 0; //for chrome, firefox, IE and opera
 }
-/*slideshow function,styling& animation*/
+
+//slideshow function,styling& animation
 var slideIndex = 0;
 showSlides();
 function showSlides() {
@@ -37,9 +39,10 @@ function showSlides() {
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 2000); /* Change image every 2 seconds*/
+    setTimeout(showSlides, 2000); //Change image every 2 seconds
 }
-/*banner text, introduce viewer to page*/
+
+//banner text, introduce viewer to page
 $("h4").css({ "color": "white", "font-style": "italic", "text-align": "center" });
 $("h4").on({
     mouseenter: function () {
@@ -49,7 +52,8 @@ $("h4").on({
         $(this).css("font-size", "2em", "z-index", "3");
     }
 });
-/*h2, visibility& interactivity*/
+
+//h2, visibility& interactivity
 $("h2").css({ "text-align": "center", "padding": "0.5em" });
 $("h2").on({
     mouseenter: function () {
@@ -59,7 +63,8 @@ $("h2").on({
         $(this).css("font-size", "1em");
     }
 });
-/*icons interactivity*/
+
+//icons interactivity
 $("i").on({
     mouseenter: function () {
         $(this).css("font-size", "2em");
@@ -68,7 +73,8 @@ $("i").on({
         $(this).css("font-size", "2em");
     }
 });
-/*topnav interactivity*/
+
+//topnav interactivity
 $("button", "top-nav").on({
     mouseenter: function () {
         $(this).css("font-size", "1.5em");
