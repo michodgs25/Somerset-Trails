b258fe5cf41108a8794a01ec54e003dc2dc5e4d7
michodgs24 <57402787+michodgs24@users.noreply.github.com>
2020-07-10 17:28:13 +0100

    Format code, increase readability

--- scripts/js/blagdon-map.js
+++ scripts/js/blagdon-map.js
@@ -1,33 +1,36 @@
 //credit to google developer documentation
 var locations = [
-    //code adapted from google developers
-    ["Blagdon Lake", 51.3348101, -2.7040838, 2],
+	//code adapted from google developers
+	["Blagdon Lake", 51.3348101, -2.7040838, 2],
 ];
 var map;
 //create map object
 function initMap() {
-    map = new google.maps.Map(document.getElementsByClassName("map-custom")[0], {
-        center: { lat: 51.3348101, lng: -2.7040838 },
-        zoom: 9
-    });
+	map = new google.maps.Map(document.getElementsByClassName("map-custom")[0], {
+		center: {
+			lat: 51.3348101,
+			lng: -2.7040838
+		},
+		zoom: 9
+	});
 
-    //call map window& create marker variable
-    var infowindow = new google.maps.InfoWindow();
+	//call map window& create marker variable
+	var infowindow = new google.maps.InfoWindow();
 
-    var marker, i;
+	var marker, i;
 
-    for (i = 0; i < locations.length; i++) {
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-            map: map
-        });
+	for (i = 0; i < locations.length; i++) {
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+			map: map
+		});
 
-        //add event listener, set markers to locations
-        google.maps.event.addListener(marker, "click", (function (marker, i) {
-            return function () {
-                infowindow.setContent(locations[i][0]);
-                infowindow.open(map, marker);
-            };
-        })(marker, i));
-    }
+		//add event listener, set markers to locations
+		google.maps.event.addListener(marker, "click", (function (marker, i) {
+			return function () {
+				infowindow.setContent(locations[i][0]);
+				infowindow.open(map, marker);
+			};
+		})(marker, i));
+	}
 }
