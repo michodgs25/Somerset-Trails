5d0908ff6f0b79fd79b5342ae90f0b114103eae0
michodgs24 <michodgs@gmail.com>
2020-07-10 16:12:57 +0000

    Format Javascript& Csscode and edit footer logo

--- scripts/js/somerset-map.js
+++ scripts/js/somerset-map.js
@@ -1,34 +1,34 @@
-      //credit to google developer documentation
-      var locations = [
-          //code adapted from google developers
-          ["Somerset Coastal Path", 51.2072165, -3.4641482, 5],
-      ];
-      
-      var map;
-      //create map object
-      function initMap() {
-          map = new google.maps.Map(document.getElementsByClassName("map-custom")[0], {
-              center: { lat: 51.180000, lng: -3.000000 },
-              zoom: 8
-          });
-          
-          //call map window& create marker variable
-          var infowindow = new google.maps.InfoWindow();
-      
-          var marker, i;
-      
-          for (i = 0; i < locations.length; i++) {
-              marker = new google.maps.Marker({
-                  position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-                  map: map
-              });
-              
-              //add event listener, set markers to locations
-              google.maps.event.addListener(marker, "click", (function (marker, i) {
-                  return function () {
-                      infowindow.setContent(locations[i][0]);
-                      infowindow.open(map, marker);
-                  };
-              })(marker, i));
-          }
-      }
+//credit to google developer documentation
+var locations = [
+    //code adapted from google developers
+    ["Somerset Coastal Path", 51.2072165, -3.4641482, 5],
+];
+
+var map;
+//create map object
+function initMap() {
+    map = new google.maps.Map(document.getElementsByClassName("map-custom")[0], {
+        center: { lat: 51.180000, lng: -3.000000 },
+        zoom: 8
+    });
+
+    //call map window& create marker variable
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        });
+
+        //add event listener, set markers to locations
+        google.maps.event.addListener(marker, "click", (function (marker, i) {
+            return function () {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            };
+        })(marker, i));
+    }
+}
